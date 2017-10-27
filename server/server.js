296574399ad7e0440d6b93336c128e77c73533d7
Alan-Miller <alan.ashman@gmail.com>
2017-10-26 20:55:48 -0600

    added my comments and edits

--- server/server.js
+++ server/server.js
@@ -1,51 +1,50 @@
 const express = require('express')
-    , bodyParser = require('body-parser')
-    , cors = require('cors')
-    , config =  require('./config')
-    , stripe = require('stripe')(config.secret_key)
-    , app =  module.exports = express();
+  , bodyParser = require('body-parser')
+  , cors = require('cors')
+  , config = require('./config')
+  , stripe = require('stripe')(config.secret_key) // invoke library with secret key
+  , app = module.exports = express();
 
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 
-app.post('/api/payment', function(req, res, next){
+app.post('/api/payment', function (req, res, next) {
   //convert amount to pennies
   const amountArray = req.body.amount.toString().split('');
+  console.log('amt', req.body.amount.toString().split(''));
+
+  // Joe's penny function below
   const pennies = [];
   for (var i = 0; i < amountArray.length; i++) {
-    if(amountArray[i] === ".") {
-      if (typeof amountArray[i + 1] === "string") {
-        pennies.push(amountArray[i + 1]);
-      } else {
-        pennies.push("0");
-      }
-      if (typeof amountArray[i + 2] === "string") {
-        pennies.push(amountArray[i + 2]);
-      } else {
-        pennies.push("0");
-      }
-    	break;
-    } else {
-    	pennies.push(amountArray[i])
+    if (amountArray[i] === ".") {
+      if (typeof amountArray[i + 1] === "string") pennies.push(amountArray[i + 1]);
+      else pennies.push("0");
+      if (typeof amountArray[i + 2] === "string") pennies.push(amountArray[i + 2]);
+      else pennies.push("0");
+      break;
     }
+    else pennies.push(amountArray[i]);
   }
   const convertedAmt = parseInt(pennies.join(''));
 
-  const charge = stripe.charges.create({
-  amount: convertedAmt, // amount in cents, again
-  currency: 'usd',
-  source: req.body.token.id,
-  description: 'Test charge from react app'
-}, function(err, charge) {
-    if (err) return res.sendStatus(500)
-    return res.sendStatus(200);
-  // if (err && err.type === 'StripeCardError') {
-  //   // The card has been declined
-  // }
-});
+  const charge = stripe.charges.create( // method built in to library
+    { 
+      amount: convertedAmt, // amount in cents, again
+      currency: 'usd',
+      source: req.body.token.id, // needs to be the token id
+      description: 'Test charge from react app' // any description you want
+    },
+    function (err, charge) {
+      if (err) return res.sendStatus(500); // error means charge failure
+      return res.sendStatus(200);
+      // if (err && err.type === 'StripeCardError') {
+      //   // The card has been declined
+      // }
+    });
 });
 
 
-app.listen(config.PORT, () => {
-  console.log('Listening to port: ', config.PORT)});
+app.listen(config.PORT, _ => {
+  console.log('Listening to port: ', config.PORT)
+});
--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import stripe from './config';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
-import StripeCheckout from 'react-stripe-checkout';
-import stripe from './stripeKey';
-import axios from 'axios';
 
 class App extends Component {
   onToken = (token) => {
-    token.card = void 0;
-    console.log('token', token);
-    axios.post('http://localhost:3535/api/payment', { token, amount: 100 } ).then(response => {
-      alert('we are in business')
-    });
+    console.log('token', token); // can check token for info, including user email
+    token.card = void 0; // remove credit card info from token so it never hits server
+    axios.post('http://localhost:3003/api/payment', { token, amount: 1000 } )
+    // when the token is sent to use from Stripe, we make a post request to make the payment
+    .then(response => { console.log('POST response', response); });
   }
 
   render() {
@@ -19,12 +19,12 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React-Stripe</h2>
         </div>
-        <StripeCheckout
-          token={this.onToken}
-          stripeKey={ stripe.pub_key }
-          amount={1000}
+        <StripeCheckout className="sc"
+          token={this.onToken} // get token back
+          stripeKey={ stripe.publishable_key }  // public key 
+          amount={1000} // required, should be same as in axios get request
         />
       </div>
     );
